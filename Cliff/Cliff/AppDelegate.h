bca1c04dd6a8ab0f77c935c97c40852c5d76b196
Alex Ryan <alex@alexryan.org>
2014-07-27 12:57:49 -0700

    initial commit

---
+++ Cliff/Cliff/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cliff
+//
+//  Created by Alex Ryan on 7/27/14.
+//  Copyright (c) 2014 Jaden Geller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cliff/Cliff/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cliff
+//
+//  Created by Alex Ryan on 7/27/14.
+//  Copyright (c) 2014 Jaden Geller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
