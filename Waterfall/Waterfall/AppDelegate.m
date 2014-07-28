9f685b5a10172a182ad18862ccd679aeff669f06
Alex Ryan <ialex.ryan@gmail.com>
2014-07-27 19:21:20 -0700

    BT changes

--- Waterfall/Waterfall/AppDelegate.m
+++ Waterfall/Waterfall/AppDelegate.m
@@ -13,6 +13,7 @@
 @property NSStatusItem *statusItem;
 @property (weak) IBOutlet NSMenu *statusMenu;
 @property NSWindowController *pairController;
+@property CBCentralManager *manager;
 
 @end
 
@@ -21,17 +22,48 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
     
+    // Status Bar
     _statusItem = [[NSStatusBar systemStatusBar]statusItemWithLength:NSVariableStatusItemLength];
     [_statusItem setImage:[NSImage imageNamed:@"menu_icon"]];
     [_statusItem setHighlightMode:YES];
     [_statusItem setMenu:self.statusMenu];
+    
+    // Notification Center
+    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver: self selector: @selector(applicationDeactivateNotification:) name: NSWorkspaceDidDeactivateApplicationNotification object: NULL];
+    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver: self selector: @selector(applicationActivateNotification:) name: NSWorkspaceDidActivateApplicationNotification object: NULL];
+
+    // Core Bluetooth
+    _manager = [[CBCentralManager alloc]initWithDelegate:self queue:nil options:@{CBCentralManagerOptionShowPowerAlertKey : @YES}];
 
 }
-
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
+
+#pragma mark - Handle Locks Screen Events
+-(void)applicationDeactivateNotification:(NSNotification*)notification{
+    if ([[notification.userInfo[@"NSWorkspaceApplicationKey"]bundleIdentifier] isEqualToString:@"com.apple.loginwindow"]) {
+        [self lockScreenDismissed];
+    }
+}
+
+-(void)applicationActivateNotification:(NSNotification*)notification{
+    if ([[notification.userInfo[@"NSWorkspaceApplicationKey"]bundleIdentifier] isEqualToString:@"com.apple.loginwindow"]) {
+        [self lockScreenAppeared];
+    }
+}
+
+-(void)lockScreenAppeared{
+
+    NSLog(@"HI");
+}
+
+-(void)lockScreenDismissed{
+    NSLog(@"BYE");
+}
+
+#pragma mark - Handle UI Events
 
 - (IBAction)addNewDevice:(id)sender
 {
@@ -47,5 +79,18 @@
 - (IBAction)quitPress:(id)sender {
     [[NSApplication sharedApplication]terminate:self];
 }
+
+#pragma mark - Core Bluetooth
+
+-(void)centralManagerDidUpdateState:(CBCentralManager *)central{
+    if (central.state == CBCentralManagerStatePoweredOn) {
+        [central scanForPeripheralsWithServices:@[[CBUUID UUIDWithString:@"98FE13EF-0596-4654-998F-FF3E1E207941"]] options:nil];
+    }
+}
+
+-(void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI{
+    
+}
+
 
 @end
--- Waterfall/Waterfall/JGAddDeviceWindowController.m
+++ Waterfall/Waterfall/JGAddDeviceWindowController.m
@@ -18,6 +18,9 @@
     [super windowDidLoad];
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    
+    
 }
+
 
 @end
